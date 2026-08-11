ba16a69fd9c88a26af53537d568575dfe0f3c976
Gaurav Kumar <gaurav.kumar@getvisitapp.com>
2026-08-11 16:20:45 +0530

    Initial Commit

---
+++ samplewebview/samplewebviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  samplewebviewApp.swift
+//  samplewebview
+//
+//  Created by Visit Health on 11/08/26.
+//
+
+import SwiftUI
+
+@main
+struct samplewebviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
